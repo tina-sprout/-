0ff27421d534dff703cbf1f0a763cec1793d5a01
tina <32284360+tina-sprout@users.noreply.github.com>
2017-12-07 11:39:47 +0900

    tax
    test

---
+++ tax.php
@@ -0,0 +1,17 @@
+<php?
+
+//taxに1.08(消費税)を代入
+
+//priceに100~8000の値を代入
+$price = rand(100,8000);
+
+
+//pretax_priceにrand*taxの結果を代入
+$pretax_price = $price * tax;
+
+$unit = "円";
+
+//出力
+echo $price.$unit."の税込みは".floor($pretax_price).$unit."です";
+
+?>
