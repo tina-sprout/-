c3faa764ca57e843b0406421258a64b7ab932e68
tina <32284360+tina-sprout@users.noreply.github.com>
2017-12-08 10:42:14 +0900

    2017/12/8/10:42

--- tax.php
+++ tax.php
@@ -3,12 +3,9 @@
 //taxに1.08(消費税)を代入
 define('tax',1.08);
 
-//priceに100~8000の値を代入
-
+//入力
 $stdin = trim(fgets(STDIN));  
 var_dump($stdin);
-
-$price =
 
 //pretax_priceにrand*taxの結果を代入
 $pretax_price = $price * tax;
