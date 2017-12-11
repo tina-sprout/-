5a0201365ab523bf1a83b130834f569411ce1528
tina <32284360+tina-sprout@users.noreply.github.com>
2017-12-11 10:23:56 +0900

    10:23

---
+++ enzanshi.php
@@ -0,0 +1,21 @@
+<php?
+    $a = 9;
+    $b = 3;
+    $c = 10;
+
+    $answer1 = $a + $b;
+    print "$aから$bを足した結果は".$answer1."<br /?";
+
+    $answer2 = $a - $b;
+    print "$aから$bを引いた結果は".$answer2."<br /?";
+
+    $answer3 = $a * $b;
+    print "$aから$bを掛けた結果は".$answer3."<br /?";
+
+    $answer4 = $a / $b;
+    print "$aから$bを割った結果は".$answer4."<br /?";
+
+    $answer5 = $a % $b;
+    print "$aから$bを割った余りは結果は".$answer5."<br /?";
+
+?>
