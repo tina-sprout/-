5f2243b7bd2c393a584aec107f0e09a75dcabbba
tina <32284360+tina-sprout@users.noreply.github.com>
2017-12-12 10:10:19 +0900

    10:10

--- if.php
+++ if.php
@@ -1,14 +1,28 @@
 <php?
+
 $a = 10;
 
-if($a === 5) {
-
-    print'$aの値は5です';
-
-}else{
-
-    print'$aの値は5以外です';
-
+switch($a) {
+    //0~4
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        print '$aの値は0~4の間の値です';
+        break;
+    //5~9
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+        print '$aの値は5~9の間の値です';
+        break;
+    //それ以外
+    default:
+        print'$aの値は0~9以外の値です';
+        break;
 }
 
 ?>
