5cedd3e7bb08e0bac1e10eb9ae8bcb809e54445c
tina <32284360+tina-sprout@users.noreply.github.com>
2017-12-13 10:57:13 +0900

    10:57

--- PasswordCreat.php
+++ PasswordCreat.php
@@ -4,8 +4,7 @@ $num = 8; //文字数
 $ar1 = range('a','z'); //小文字を配列
 $ar2 = range('A','Z'); //大文字を配列に
 $ar3 = range(0,9); //数字を配列に
-$ar4 = range('#','%','&');//記号の配列
-$ar_all = array_merge($ar1,$ar2,$ar3,$ar4); //全て結合
+$ar_all = array_merge($ar1,$ar2,$ar3); //全て結合
 
 shuffle($ar_all); //ランダムにシャッフル
 
